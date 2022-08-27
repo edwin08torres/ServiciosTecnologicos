2054ad07a7b568ab88769894b7a40d521ab349ca
edwin08torres <at2899743@gmail.com>
2022-08-27 15:02:20 -0600

    Se ha agregado el proyecto

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// firsty
+function cardToggle() {
+    var popup = document.querySelector('.yoquese');
+    popup.classList.toggle('active');
+
+    var popup2 = document.querySelector('.sise');
+    popup2.classList.toggle('active');
+
+    var popup3 = document.querySelector('.change');
+    popup3.classList.toggle('color');
+
+    var popup4 = document.querySelector('.change2');
+    popup4.classList.toggle('color');
+}
+
+// const Abrir =
+// document.querySelector("#Abrir");
+
+// const modal =
+// document.querySelector("#modal");
+
+// Abrir.addEventListener("click",()=>{
+//     modal.showModal();
+// })
+
